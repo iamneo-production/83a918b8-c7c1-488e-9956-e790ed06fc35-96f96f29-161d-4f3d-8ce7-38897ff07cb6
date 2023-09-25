ddd14d0d78f2ca101e2e0a62ad3ee58a3069766f
DivakarSathees <divakarsathees7@gmail.com>
2023-09-25 14:02:48 +0000

    initial commit dotnetapp

---
+++ dotnetapp/Models/Team.cs
@@ -0,0 +1,11 @@
+﻿namespace dotnetapp.Models
+{
+    public class Team
+    {
+        public long Id { get; set; }
+        // Write your team class here...
+        public string Name { get; set; }
+        public double MaximumBudget { get; set; }
+
+    }
+}
